d17f973111f3e4c64765dbee42183b4d0583cc7c
m33ls <70194930+m33ls@users.noreply.github.com>
2025-02-02 22:02:53 -0800

    vga text buffer macros etc

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
+// Print to VGA text buffer on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_buf();
+    println!("Hello World!");
+    panic!("Test message");
 
     loop {}
 }
