c4a1c7fa045c64ca6e8b79dbee67ca53fc773737
m33ls <70194930+m33ls@users.noreply.github.com>
2025-02-05 11:25:52 -0800

    implement tests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 // Print to VGA text buffer on panic
 #[panic_handler]
@@ -15,6 +26,10 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World!");
+
+    #[cfg(test)]
+    test_main();
+
     panic!("Test message");
 
     loop {}
