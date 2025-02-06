8f36cad3b427fd1cd24d33f50ff489455032f51a
m33ls <70194930+m33ls@users.noreply.github.com>
2025-02-05 23:09:10 -0800

    qemu exit device

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,30 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed  = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 // Print to VGA text buffer on panic
@@ -30,7 +54,7 @@ pub extern "C" fn _start() -> ! {
     #[cfg(test)]
     test_main();
 
-    panic!("Test message");
+    //panic!("Test message");
 
     loop {}
 }
