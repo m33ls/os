6da060925d1e6efc40248489b749255092b33d26
m33ls <70194930+m33ls@users.noreply.github.com>
2025-02-08 14:35:09 -0800

    double fault handler and global descriptor table

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,11 +1,15 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use crate::println;
+use crate::{gdt, println};
 use lazy_static::lazy_static;
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
         idt
     };
 }
@@ -20,8 +24,15 @@ extern "x86-interrupt" fn breakpoint_handler(
     println!("Exception: Breakpoint\n{:#?}", stack_frame);
 }
 
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame, _error_code: u64) -> !
+{
+    panic!("Exception:  Double Fault\n{:#?}", stack_frame);
+}
+
 #[test_case]
 fn test_breakpoint_exception() {
     // invokes a breakpoint exception
     x86_64::instructions::interrupts::int3();
 }
+
