0712bfde2361507635235aa60bf50459e53262d8
m33ls <70194930+m33ls@users.noreply.github.com>
2025-04-01 12:05:33 -0700

    paging implementation

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
 //use os::println;
 
 mod vga_buffer;
@@ -25,19 +26,39 @@ fn panic(info: &PanicInfo) -> ! {
     os::test_panic_handler(info)
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use os::memory::translate_addr;
+    use x86_64::{VirtAddr, structures::paging::Page};
+    use x86_64::structures::paging::PageTable;
+
     println!("Hello World!");
 
     os::init();
 
-    // use x86_64::registers::control::Cr3;
-    // let (level_4_page_table, _) = Cr3::read();
-    // println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physcial address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
 
+    println!("Success!");
     //panic!("Test message");
 
     //loop {}
